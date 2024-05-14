a2c8373f19e906d674bfab6c35d8c34feb706a39
alialtunar <altunarali284@gmail.com>
2024-05-14 19:54:13 +0300

    google-ui preparing

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,22 @@
+import HomeHeader from '@/components/HomeHeader'
+import HomeSearch from '@/components/HomeSearch'
+import Image from 'next/image'
 import React from 'react'
 
 function page() {
   return (
-    <h1 className='text-red-500'>
-      home
-    </h1>
+    <>
+      <HomeHeader/>
+      <div className='flex flex-col items-center mt-24'>
+        <Image
+         width={300}
+         height={100}
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png" alt='google image' style={{width:'auto'}} priority/>
+
+              <HomeSearch/>
+      </div>
+
+    </>
   )
 }
 
