1f44e4f7f78f650f3fa4fa4558097f6b94495f4f
alialtunar <altunarali284@gmail.com>
2024-05-15 17:06:33 +0300

    Add-image-results

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,10 @@
-import { Inter } from "next/font/google";
+
 import "./globals.css";
 import Footer from "../components/Footer";
 
 
 
-const inter = Inter({ subsets: ["latin"] });
+
 
 export const metadata = {
   title: "Google Clone",
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="relative min-h-screen">
         {children}
         <Footer/>
       </body>
--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import SearchBox from '../components/SearchBox'
+import {RiSettings3Line} from 'react-icons/ri'
+import { TbGridDots } from 'react-icons/tb'
+import SearchHeaderOptions from '../components/SearchHeaderOptions'
+
 
 const SearchHeader = () => {
   return (
-    <div>
-      search header
+   <header className='sticky top-0 bg-white '>
+    <div className="flex w-full p-6 items-center justify-between">
+      <Link href='/'>
+      <Image
+         width={120}
+         height={40}
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png" alt='google image' style={{width:'auto'}} priority/>
+      </Link>
+      <div className="flex-1">
+        <SearchBox/>
+      </div>
+      <div className="hidden md:inline-flex space-x-2">
+        <RiSettings3Line className='bg-transparent hover:bg-gray-300 p-2 text-4xl rounded-full cursor-pointer' />
+        <TbGridDots className='bg-transparent hover:bg-gray-300 p-2 text-4xl rounded-full cursor-pointer'/>
+      </div>
+      <button className='bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow ml-2'>Sign in</button>
     </div>
+    <SearchHeaderOptions/>
+   </header>
   )
 }
 
